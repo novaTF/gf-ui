752932eadbbe81e2caa019b43981df092a59e746
lococo <lococo.exe@gmail.com>
2016-04-27 16:33:00 +0800

    add: isToggleMod to dropmenu.

--- ng2/menu/menu.component.ts
+++ ng2/menu/menu.component.ts
@@ -8,42 +8,34 @@ import {DropmenuComponent} from  '../dropmenu/dropmenu.component';
     styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-    @Input()
-    title:Object;
-
-    @Input()
-    items:Array<Object>;
-
-    /**
-     * 激活状态
-     */
-    @Input()
-    active:boolean;
-
-    /**
-     * 是否水平显示
-     */
-    @Input()
-    horizon:boolean;
-
-    @Input()
-    selected:string;
+    @Input() title:Object;
+    @Input() items:Array<Object>;
+    @Input() active:boolean; // 是否展开
+    @Input() horizon:boolean; // 是否水平展示
+    @Input() isToggleMod:boolean; // 如果开启toggle模式, 再次点击已选中的item可以取消选中
+    @Input() selected:any;
 
     @Output()
     onSelect:EventEmitter<Event> = new EventEmitter();
 
     constructor(private dropmenu:DropmenuComponent) {
-        dropmenu.addMenu(this);
+      dropmenu.addMenu(this);
     }
 
     select(item:Object) {
+      this.active = false; //选择后关闭面板
+      if (item.value === this.selected && !this.isToggleMod) return; //如果已选中, 并且未开启toggle模式, 则不做处理
+
+      if (item.value === this.selected && this.isToggleMod) {
+        this.selected = undefined;  //如果已经选中, 并且开启toggle模式, 设选中元素为undefined.
+      } else {
         this.selected = item.value;
-        //选择后关闭面板
-        this.active = false;
-        //触发事件
-        let event:any = new Event('onSelect');
-        event.value = this.selected;
-        event.field = this.title.value;
-        this.onSelect.emit(event);
+      }
+
+      //触发事件
+      let event:any = new Event('onSelect');
+      event.value = this.selected;
+      event.field = this.title.value;
+      this.onSelect.emit(event);
     }
 }
